525b20a04e154fef49a6fd8363116ff7662f35ed
Devon Martens <devonmartens@Devons-MacBook-Pro-2.local>
2021-07-16 11:07:11 -0400

    adds corresponding variables to tests, to solidity contract

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -14,8 +14,8 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
     // symbol: Is a short string like 'USD' -> 'American Dollar'
-    
-
+    string public constant name = "Udacity Token";
+    string public constant symbol = "UDC"
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
     // mapping the TokenId and price
